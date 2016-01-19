807fca969728b71c10af8d3eaa5221674c9f052a
Jens Schumann <schumajs@gmail.com>
2016-01-19 15:13:05 +0100

    Remove unused vendor scripts; add google c/c++ styleguide.

--- init.el
+++ init.el
@@ -42,14 +42,18 @@
 (package-initialize)
 
 (add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
 ;; auto installed packaged
 (defvar prelude-packages
     '(ack-and-a-half clojure-mode expand-region haskell-mode markdown-mode
-                     paredit python rainbow-mode solarized-theme
+                     paredit python rainbow-delimiters solarized-theme
                      volatile-highlights yari zenburn-theme go-mode recentf
-                     auto-complete cc-mode cider company csharp-mode)
+                     auto-complete auto-complete-clang cc-mode cider company
+                     csharp-mode leerzeichen flycheck js2-mode json-mode
+                     exec-path-from-shell markdown-mode)
   "A list of packages to ensure are installed at launch.")
 
 (require 'cl)
@@ -77,30 +81,25 @@
 (add-to-list 'load-path "~/.emacs.d/vendor")
 
 ;; sync $PATH
-(defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (replace-regexp-in-string
-                          "[ \t\n]*$"
-                          ""
-                          (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-    (setenv "PATH" path-from-shell)
-    (setq eshell-path-env path-from-shell) ; for eshell users
-    (setq exec-path (split-string path-from-shell path-separator))))
+;;(defun set-exec-path-from-shell-PATH ()
+;;  (let ((path-from-shell (replace-regexp-in-string
+;;                          "[ \t\n]*$"
+;;                          ""
+;;                          (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+;;    (setenv "PATH" path-from-shell)
+;;    (setq eshell-path-env path-from-shell) ; for eshell users
+;;    (setq exec-path (split-string path-from-shell path-separator))))
+;;
+;; (when window-system (set-exec-path-from-shell-PATH))
 
-(when window-system (set-exec-path-from-shell-PATH))
+;; https://github.com/purcell/exec-path-from-shell
+;; only need exec-path-from-shell on OSX
+;; this hopefully sets up path and other vars better
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
 
-;; dirtree
-(require 'dirtree)
-
-;; auto-complete mode
-(require 'auto-complete-config)
-(ac-config-default)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/vendor/auto-complete/dict")
-
-;; go mode
+;; go
 ;; (add-hook 'before-save-hook #'gofmt-before-save)
-
-;; go mode + auto-complete
-(add-to-list 'ac-modes 'go-mode)
 
 ;; cider
 (add-hook 'cider-mode-hook #'eldoc-mode)
@@ -116,10 +115,10 @@
 ;; paredit + cider
 (add-hook 'cider-repl-mode-hook #'paredit-mode)
 
-;; rainbow + cider
-(add-hook 'cider-repl-mode-hook #'rainbow-mode)
+;; rainbow
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 
-;; company mode + cider
+;; company + cider
 (add-hook 'cider-repl-mode-hook #'company-mode)
 (add-hook 'cider-mode-hook #'company-mode)
 
@@ -138,12 +137,39 @@
 (global-set-key (kbd "C-x <right>") 'windmove-right)
 (global-set-key (kbd "C-x <left>") 'windmove-left)
 
-;; tab width
+;; google c/c++ style guide
+(require 'google-c-style)
+(add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c-mode-common-hook 'google-make-newline-indent)
 
-(setq tab-width 4) ; or any other preferred value
-
-;; c mode
-
+;; ... + customizations
 (defvaralias 'c-basic-offset 'tab-width)
 (setq c-basic-indent 4)
 
+;; leerzeichen
+(require 'leerzeichen)
+(add-hook 'css-mode-hook #'rainbow-delimiters-mode)
+
+;; auto-complete
+(require 'auto-complete-config)
+(ac-config-default)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/vendor/auto-complete/dict")
+(add-to-list 'ac-modes 'cc-mode)
+(add-to-list 'ac-modes 'go-mode)
+(add-to-list 'ac-modes 'clojure-mode)
+(add-to-list 'ac-modes 'css-mode)
+(add-to-list 'ac-modes 'js2-mode)
+(add-to-list 'ac-modes 'json-mode)
+(add-to-list 'ac-modes 'web-mode)
+
+;; auto-complete-clang
+(require 'auto-complete-clang)
+
+;; http://www.flycheck.org/manual/latest/index.html
+(require 'flycheck)
+
+;; turn on flychecking globally
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; use c++11 when flychecking
+(add-hook 'c++-mode-hook (lambda () (setq flycheck-gcc-language-standard "c++11")))
