f6a07af2cd075c12f9f65544d11606ddd8c25a9d
Jens Schumann <schumajs@gmail.com>
2017-04-04 16:25:56 +0200

    Add toml-mode.

--- init.el
+++ init.el
@@ -321,7 +321,6 @@
 
 (add-hook 'post-command-hook 'show-fly-err-at-point)
 
-(require 'toml-mode)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -336,3 +335,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(require 'toml-mode)
